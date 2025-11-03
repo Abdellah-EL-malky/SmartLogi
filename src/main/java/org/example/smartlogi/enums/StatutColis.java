057f253d8470a7f03ea4a2242cc68350dabdb8f0
Abdellah <abdel.elmalky@gmail.com>
2025-11-03 11:12:01 +0100

    enum statut colis

--- src/main/java/org/example/smartlogi/enums/StatutColis.java
+++ src/main/java/org/example/smartlogi/enums/StatutColis.java
@@ -1,4 +1,19 @@
 package org.example.smartlogi.enums;
 
 public enum StatutColis {
+    CREE("Créé"),
+    COLLECTE("Collecté"),
+    EN_STOCK("En stock"),
+    EN_TRANSIT("En transit"),
+    LIVRE("Livré");
+
+    private final String libelle;
+
+    StatutColis(String libelle) {
+        this.libelle = libelle;
+    }
+
+    public String getLibelle() {
+        return libelle;
+    }
 }
