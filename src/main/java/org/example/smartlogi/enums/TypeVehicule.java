567aa5648638a57c5bf1fbb0529e068dec72738c
Abdellah <abdel.elmalky@gmail.com>
2025-11-03 11:13:11 +0100

    enum type vehicule

--- src/main/java/org/example/smartlogi/enums/TypeVehicule.java
+++ src/main/java/org/example/smartlogi/enums/TypeVehicule.java
@@ -1,4 +1,17 @@
 package org.example.smartlogi.enums;
 
 public enum TypeVehicule {
+    VOITURE("Voiture"),
+    CAMIONNETTE("Camionnette"),
+    MOTO("Moto");
+
+    private final String libelle;
+
+    TypeVehicule(String libelle) {
+        this.libelle = libelle;
+    }
+
+    public String getLibelle() {
+        return libelle;
+    }
 }
