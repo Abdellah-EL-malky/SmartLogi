043f3dbcda271655bce00df0257a00eaf45132b2
Abdellah <abdel.elmalky@gmail.com>
2025-11-03 11:12:50 +0100

    enum priorité colis

--- src/main/java/org/example/smartlogi/enums/PrioriteColis.java
+++ src/main/java/org/example/smartlogi/enums/PrioriteColis.java
@@ -1,4 +1,17 @@
 package org.example.smartlogi.enums;
 
 public enum PrioriteColis {
+    NORMALE("Normale"),
+    URGENTE("Urgente"),
+    TRES_URGENTE("Très urgente");
+
+    private final String libelle;
+
+    PrioriteColis(String libelle) {
+        this.libelle = libelle;
+    }
+
+    public String getLibelle() {
+        return libelle;
+    }
 }
