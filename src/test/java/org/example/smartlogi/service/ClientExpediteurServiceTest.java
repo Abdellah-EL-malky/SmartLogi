f58a47a926d44329103f75c866ff0ce38f41db2e
Abdellah <abdel.elmalky@gmail.com>
2025-11-11 16:24:33 +0100

    config test et création des premiers testes

---
+++ src/test/java/org/example/smartlogi/service/ClientExpediteurServiceTest.java
@@ -0,0 +1,43 @@
+//package org.example.smartlogi.service;
+//
+//import org.example.smartlogi.dto.ClientExpediteurDTO;
+//import org.example.smartlogi.mapper.ClientExpediteurMapper;
+//import org.example.smartlogi.repository.ClientExpediteurRepository;
+//import org.junit.jupiter.api.Test;
+//import org.mockito.InjectMocks;
+//import org.mockito.Mock;
+//
+//public class ClientExpediteurServiceTest {
+//
+//    @Mock
+//    private ClientExpediteurRepository clientExpediteurRepository;
+//
+//    private ClientExpediteurMapper clientExpediteurMapper;
+//
+//    @InjectMocks
+//    private ClientExpediteurService clientExpService;
+//
+//    @Test
+//    public void createClient_withSuccess(){
+//        //Arrange
+//        ClientExpediteurDTO dtoRequest = new ClientExpediteurDTO();
+//
+//        public void setNom(String nom) {this.nom = nom;}
+//
+//        public void setPrenom(String prenom) {this.prenom = prenom;}
+//
+//        public void setEmail(String email) {this.email = email;}
+//
+//        public void setTelephone(String telephone) {this.telephone = telephone;}
+//
+//        public void setAdresse(String adresse) {this.adresse = adresse;}
+//
+//    }
+//
+//    @Test
+//    public void createClient_withError(){
+//
+//    }
+//
+//
+//}
