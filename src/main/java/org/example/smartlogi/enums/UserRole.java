363b5fbf08c89bd7f69944d4076b2cf00eaec9a3
Abdellah <abdel.elmalky@gmail.com>
2025-11-14 12:00:00 +0100

    pom.xml + userEntity + userRepository+

--- src/main/java/org/example/smartlogi/enums/UserRole.java
+++ src/main/java/org/example/smartlogi/enums/UserRole.java
@@ -1,4 +1,7 @@
 package org.example.smartlogi.enums;
 
 public enum UserRole {
+    ROLE_MANAGER,
+    ROLE_DELIVERY_PERSON,
+    ROLE_CLIENT
 }
--- src/main/java/org/example/smartlogi/repository/UserRepository.java
+++ src/main/java/org/example/smartlogi/repository/UserRepository.java
@@ -1,4 +1,14 @@
 package org.example.smartlogi.repository;
 
-public class UserRepository {
+import org.example.smartlogi.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
 }
